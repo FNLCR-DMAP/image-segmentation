d609148666bed09452ed6282ac7df96af3a8d87f
George Zaki <george.zaki@nih.gov>
2019-06-21 12:37:46 -0400

    Added placeholder to set IMGAUG

--- packages/image_segmentation/test/utils/run_test.sh
+++ packages/image_segmentation/test/utils/run_test.sh
@@ -3,7 +3,13 @@
 my_dir=$(cd `dirname $0` && pwd)
 
 src_dir=$my_dir/../../../
-imgaug_dir=/data/HiTIF/data/dl_segmentation_input/utils/imgaug
+#TODO: Set imageaug directory or source conda env
+if [ -z "$IMGAUG" ]
+then
+    echo "ERROR: IMGAUG variable must be set" >&2
+    exit 1
+fi
+imgaug_dir=$IMGAUG
 export PYTHONPATH=$PYTHONPATH:$src_dir:$imgaug_dir
 
 if [ -z  "$1" ]
